7fc956f1823b7fe58a5b6c62b2a6afc40b346b8e
Raman Zalomau <romchik349@gmail.com>
2023-10-26 22:02:47 +0300

    Done LR1 with no report

---
+++ LR1/main.py
@@ -0,0 +1,20 @@
+from PIL import Image
+from src.image_processors import median_filter, find_contours, image_equalize
+import numpy as np
+
+
+if __name__ == "__main__":
+    original_image = Image.open("flip.jpg")
+
+    # Median filter test
+    filtered_image = median_filter(original_image, 4)
+    filtered_image.save("filtered_image.jpg")
+
+    # Edge highlighting test
+    grayscale_image = filtered_image.convert('L')
+    image_with_edges = find_contours(grayscale_image, gradient_threshold=10)
+    image_with_edges.save('edge_image.jpg')
+
+    # Equalizing test
+    equalized_image = image_equalize(filtered_image)
+    equalized_image.save('equalized_image.jpg')
--- LR1/src/image_processors.py
+++ LR1/src/image_processors.py
@@ -1,30 +1,72 @@
+import PIL.Image
 import numpy as np
 from PIL import Image
+import math
 
 
-def median_filter(image, kernel_size):
-    """Median filter. 'image' is array of pixels. 'kernel_size' - how much neighbours of pixel we will filter"""
+def median_filter(image: PIL.Image.Image, kernel_size) -> PIL.Image.Image:
+    # Pillow image to array of pixels
+    image_pixels = np.array(image)
 
     # Filtered image pixels matrix
-    filtered_image = np.zeros_like(image)
+    filtered_image = np.zeros_like(image_pixels)
 
     # Filtering
-    for i in range(image.shape[0]):
-        for j in range(image.shape[1]):
+    for i in range(image_pixels.shape[0]):
+        for j in range(image_pixels.shape[1]):
             # Get the pixels in pixel neighbourhood
-            neighbors = image[max(i - kernel_size // 2, 0):min(i + kernel_size // 2 + 1, image.shape[0]),
-                              max(j - kernel_size // 2, 0):min(j + kernel_size // 2 + 1, image.shape[1])]
+            neighbors = image_pixels[max(i - kernel_size // 2, 0):min(i + kernel_size // 2 + 1, image_pixels.shape[0]),
+                        max(j - kernel_size // 2, 0):min(j + kernel_size // 2 + 1, image_pixels.shape[1])]
+            neighbors = np.concatenate(neighbors)
 
             # Sorting pixels
-            sorted_neighbors = np.sort(neighbors.reshape(-1))
+            sorted_neighbors = neighbors[np.lexsort((neighbors[:, 0], neighbors[:, 1], neighbors[:, 2]))]
 
             # Find median
             filtered_image[i][j] = sorted_neighbors[kernel_size // 2]
 
-    return filtered_image
+    return Image.fromarray(filtered_image)
 
 
-if __name__ == "__main__":
-    image = np.array(Image.open("flip.jpg"))
-    filtered_image = median_filter(image, 20)
-    Image.fromarray(filtered_image).save("filtered_image.jpg")
+def find_contours(grayscale_image: PIL.Image.Image, gradient_threshold: int = 10) -> PIL.Image.Image:
+    image_pixels = np.array(grayscale_image)
+
+    for i in range(image_pixels.shape[0] - 1):
+        for j in range(image_pixels.shape[1] - 1):
+            up_pixel = image_pixels[i][j + 1]
+            right_pixel = image_pixels[i + 1][j]
+            current_pixel = image_pixels[i][j]
+
+            dx = abs(right_pixel - current_pixel)
+            dy = abs(up_pixel - current_pixel)
+
+            gradient = min(255, math.sqrt(dx**2 + dy**2))
+            if gradient > gradient_threshold:
+                image_pixels[i][j] = 255
+            else:
+                image_pixels[i][j] = 0
+
+    return Image.fromarray(image_pixels, mode='L')
+
+
+def image_equalize(image: PIL.Image.Image) -> PIL.Image.Image:
+    image_pixels = np.array(image)
+
+    hist = np.histogram(image_pixels, bins=256)
+
+    # Вычисление PDF гистограммы
+    pdf = hist[0] / hist[0].sum()
+
+    # Интегрирование PDF гистограммы
+    cdf = np.cumsum(pdf)
+
+    # Нахождение нового значения яркости для каждого пикселя изображения
+    new_image_array = np.zeros_like(image_pixels)
+    for i in range(image_pixels.shape[0]):
+        for j in range(image_pixels.shape[1]):
+            new_image_array[i, j] = cdf[image_pixels[i, j]] * 255
+
+    # Создание нового изображения
+    new_image = Image.fromarray(new_image_array.astype(np.uint8))
+    return new_image
+
